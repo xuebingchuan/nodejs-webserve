4081e0df7ba2628a1d09f0abd898e3bb146af61c
c-xuebingchuan <123456.xue>
2021-10-28 16:03:43 +0800

    10.28功能调整为规范静态资源及页面,并批量处理托管的页面

--- appeach.js
+++ appeach.js
@@ -1,26 +1,23 @@
 ﻿var http = require('http');
 let statica = require('publlicMethods') //根据路由进行静态资源托管
 let check = require('check')       //托管的是一个项目,直接打开html文件
-let publicData = require('./src/utils/publicData')
+let publicData = require('./src/utils/publicData')  //封装的用于处理文件路径的vuex
+let fileName = require('projectFileName') //所有要托管的文件名的集合数组
 http.createServer(function (req, res) {
     let route = req.url
-    console.log(route,'---','8')
+    // console.log(route,'---','路由',fileName.fileNameArrary,'集合数组')
     if (route != '/favicon.ico '){
-        // let fileName = ''
-        // publicData.publicData.filename = ''
         if (route === '/') {
+            //首页
             publicData.totalData.setPublicData('filename','airpord')
             check.careContent(req,res,'airpord')
-        }else if (route === '/like'){
-            publicData.totalData.setPublicData('filename','like')
-            check.careContent(req,res,'like')
+        }else if (fileName.fileNameArrary.findIndex(item=>route===`/${item}`) !== -1){
+            publicData.totalData.setPublicData('filename',route.slice(1,route.length))
+            check.careContent(req,res,route.slice(1,route.length))
         }else if (route.indexOf('/static') != -1) {
             //单纯访问静态资源
             statica.careContent(req,res,'')
-        }else  if (route === '/line') {
-            publicData.totalData.setPublicData('filename','line')
-            check.careContent(req,res,'line')
-        }else{
+        }else  {
             //托管项目所依赖的静态资源
             statica.careContent(req,res,publicData.totalData.publicData.filename)
         }
