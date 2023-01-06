9797b34d4fa1bc2a2071220e1332241dcf7535fa
薛冰川 <>
2023-01-07 01:02:11 +0800

    updata:优化文件上传兼容多种数据格式

--- utils/express/index.js
+++ utils/express/index.js
@@ -1,44 +1,51 @@
+var express = require('express');
+var app = express()
+var router = express.Router();
+var multiparty = require('multiparty');//使用multiparty这个库文件,解析从客户端提交的本地文件
+var util = require('util');//node的工具类
+var fs = require('fs');
 
-const express = require('express');
-const server = express();
-const fs = require('fs');
-const multer = require('multer');
-const bodyParser = require('body-parser');
-//配置静态资源文件
-server.use(express.static(__dirname +"/public"));
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(multer({ dest: '/tmp/'}).array('file'));
+/* 上传页面 */
+router.get('/', function (req, res, next) {
+    res.sendFile(__dirname+'/'+'index.html');
+});
 
-server.get('/index.html', function (req, res) {
-    console.log(__dirname);
-    res.sendFile( __dirname + "/" + "index.html" );
-})
+/* 上传*/
+router.post('/file/uploading', function (req, res, next) {
+    //生成multiparty对象，并配置上传目标路径
+    var form = new multiparty.Form({uploadDir: __dirname + '/public/'});
+    console.log(form)
+    //上传完成后处理
+    form.parse(req, function (err, fields, {files}) {
+        console.log(err,fields,files)
+        var filesTmp = JSON.stringify(files, null, 2);
 
-server.post('/fileUpload',(req,respon)=>{
-    console.log(req.files[0]);
-    let fileName = Buffer.from(req.files[0].originalname, "latin1").toString(
-        "utf8"
-    );
-    var fileUrl = __dirname + "/public/" + fileName; //文件名
-    console.log(fileUrl);
-    fs.readFile(req.files[0].path,(err,res)=>{
-        fs.writeFile(fileUrl, res,(err=> { //文件写入
-            if( err ){
-                console.log( err );
-            }else{
-                // 文件上传成功，respones给客户端
-                response = {
-                    message:'File uploaded successfully',
-                    filename:fileName
-                };
-            }
-            console.log('/public/'+response.filename);
-            console.log(respon)
-            respon.send(JSON.stringify({msg:'上传成功',code:0,data:JSON.stringify('/public/'+response.filename)}))//返回自定义数据,比如json或者404
-        }))
-    })
-})
+        if (err) {
+            console.log('parse error: ' + err);
+        } else {
+            console.log('parse files: ' + filesTmp);
+            var inputFile = files[0];
+            var uploadedPath = inputFile.path;
+            var dstPath = __dirname + '/public/files' + inputFile.originalFilename;
+            //重命名为真实文件名
+            fs.rename(uploadedPath, dstPath, function (err) {
+                if (err) {
+                    console.log('rename error: ' + err);
+                } else {
+                    console.log('rename ok');
+                }
+            });
+            res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
+            res.write('Response data:\n\n');
+            // res.end(util.inspect({fields: fields, files: filesTmp}));
+            res.end(JSON.stringify({code:200,msg:'上传成功',data:{
+                    path: '/public/files' + inputFile.originalFilename
+                }}))
+        }
+    });
+});
+app.use(router)
 
-server.listen(8000,err=>{
-    err ? console.log(err): console.log('server http:127.0.0.1:8000');
+app.listen(8000, function () {
+    console.log('http://127.0.0.1:8000')
 })
