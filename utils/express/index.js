c66aee75830f3c3b35e95f9469056da2922edeed
薛冰川 <6569645+xueBingChuan@user.noreply.gitee.com>
2023-01-31 17:41:01 +0800

    updata:添加备注,规范代码格式

--- utils/express/index.js
+++ utils/express/index.js
@@ -1,50 +1,36 @@
-var express = require('express');
-var app = express()
-var router = express.Router();
-var multiparty = require('multiparty');//使用multiparty这个库文件,解析从客户端提交的本地文件
-var util = require('util');//node的工具类
-var fs = require('fs');
+// 使用express构建服务器api
+const express = require("express");
+// 引入上传文件逻辑代码
+const upload = require("./upload_file");
+const bodyParser = require('body-parser')
+const app = express();
 
+// 处理所有响应，设置跨域
+app.all("*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With ");
+    res.header("X-Powered-By", " 3.2.1");
+    if (req.url == '/'){
+        res.header("Content-Type", "text/html;charset=utf-8");
+    }else {
+        res.header("Content-Type", "application/json;charset=utf-8");
+    }
+    next();
+});
 /* 上传页面 */
-router.get('/', function (req, res, next) {
+app.get('/', function (req, res, next) {
     res.sendFile(__dirname+'/'+'index.html');
 });
+app.use(bodyParser.json({ type: "application/*+json" }));
+// 视频上传（查询当前切片数）
+app.post("/getSize", upload.getSize);
+// 视频上传接口
+app.post("/video", upload.video);
 
-/* 上传*/
-router.post('/file/uploading', function (req, res, next) {
-    //生成multiparty对象，并配置上传目标路径
-    var form = new multiparty.Form({uploadDir: __dirname + '/public/'});
-    //上传完成后处理
-    form.parse(req, function (err, fields, {files}) {
-        var filesTmp = JSON.stringify(files, null, 2);
-
-        if (err) {
-            console.log('parse error: ' + err);
-        } else {
-            console.log('parse files: ' + filesTmp);
-            var inputFile = files[0];
-            var uploadedPath = inputFile.path;
-            var dstPath = __dirname + '/public/' + inputFile.originalFilename;
-            console.log(uploadedPath,dstPath)
-            //重命名为真实文件名
-            fs.rename(uploadedPath, dstPath, function (err) {
-                if (err) {
-                    console.log('rename error: ' + err);
-                } else {
-                    console.log('rename ok');
-                }
-            });
-            res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
-            res.write('Response data:\n\n');
-            // res.end(util.inspect({fields: fields, files: filesTmp}));
-            res.end(JSON.stringify({code:200,msg:'上传成功',data:{
-                    path: '/public/' + inputFile.originalFilename
-                }}))
-        }
-    });
+// 开启本地端口侦听
+app.listen(8080,function () {
+    console.log('启动成功了')
 });
-app.use(router)
 
-app.listen(8000, function () {
-    console.log('http://127.0.0.1:8000')
-})
